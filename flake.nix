a22c081745c6f1ce362aa939546a3f42d03eb537
colin <colinrozzi@gmail.com>
2026-07-26 08:45:56 -0400

    release v0.12.6

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
 
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "packr";
-          version = "0.12.5";
+          version = "0.12.6";
 
           src = ./.;
 
